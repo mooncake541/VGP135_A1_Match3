5ca08a5619b22cefa39aed0e5f5c41b4013374cf
JohnWu <wusiyuan54111@gmail.com>
2019-12-06 12:57:00 -0800

    [VGP135_Dec.06] - move to new repository

---
+++ Match 3/Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+    public GameObject[] dots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initialize()
+    {
+        int dotToUse = Random.Range(0, dots.Length);
+        GameObject dot= Instantiate(dots[dotToUse], transform.position, Quaternion.identity);
+        dot.transform.parent = this.transform;
+        dot.name = this.gameObject.name;
+    }
+
+}
---
+++ Match 3/Assets/Scripts/Board.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        for (int i= 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "( " + i + ", " + j + " )";
+            }
+        }
+    }
+}
